4d7ef9fb580036849e3f1f0a3cff6ea62cafecc7
Jenny Gao <gao.jenny9@gmail.com>
2020-08-03 23:35:42 +0000

    added route parameters and template

--- express_server.js
+++ express_server.js
@@ -5,6 +5,7 @@ const PORT = 3000;
 app.set("view engine", "ejs");
 
 const urlDatabase = {
+  
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -16,7 +17,14 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase }
   res.render("urls_index", templateVars);
-})
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  // req.params.shortURL === userinput after urls/
+  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
